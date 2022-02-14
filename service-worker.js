f22ff60bb99df25c5774899a78345c462133a54a
gensir <lzjg1314@163.com>
2022-02-14 18:44:31 +0800

    update sw.js

--- service-worker.js
+++ service-worker.js
@@ -1,1 +1,1 @@
-self.addEventListener("install",e=>{e.waitUntil(()=>console.info("\u5B89\u88C5\u5B8C\u6210"))}),addEventListener("message",e=>{console.log(`The client sent me a message: ${e.data}`),e.source.postMessage("Hi client")});
+self.ASSETS=["https://gensir.github.io/welcome/index.html"],self.addEventListener("install",e=>{e.waitUntil(()=>console.info("\u5B89\u88C5\u5B8C\u6210"))}),addEventListener("message",e=>{console.log(`The client sent me a message: ${e.data}`),e.source.postMessage("Hi client")});
